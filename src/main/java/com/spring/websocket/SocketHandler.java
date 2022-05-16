7a2eea3fe2b6acb67234b59157392ce2629ff6d9
Svahnen <svahn.jonny@gmail.com>
2022-05-16 11:19:45 +0200

    Websocket example

--- src/main/java/com/spring/websocket/SocketHandler.java
+++ src/main/java/com/spring/websocket/SocketHandler.java
@@ -1,10 +1,16 @@
+package com.spring.websocket;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import com.google.gson.Gson;
+
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 @Component
 public class SocketHandler extends TextWebSocketHandler {
--- src/main/java/com/spring/websocket/WebSocketConfig.java
+++ src/main/java/com/spring/websocket/WebSocketConfig.java
@@ -1,6 +1,9 @@
 package com.spring.websocket;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
 @EnableWebSocket
